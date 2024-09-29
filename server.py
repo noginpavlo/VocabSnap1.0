65a6eaf4912be0cac11a73f9172e66aba0d1bc3a
Nohin Pavlo <nogin.pavlo@lll.kpi.ua>
2024-09-29 21:58:10 +0300

    New fitures added. Budges added. Card creation added.

--- server.py
+++ server.py
@@ -26,23 +26,38 @@ def learn_front():
 
 @app.route("/easy_scale", methods=["POST"])
 def easy_scale():
-    title = request.form.get('w_title')
+    title = request.form.get("w_title")
     extractor.easy_scale(title)
-    return redirect(url_for('learn_front'))
+    return redirect(url_for("learn_front"))
 
 
 @app.route("/medium_scale", methods=["POST"])
 def medium_scale():
-    title = request.form.get('w_title')
+    title = request.form.get("w_title")
     extractor.medium_scale(title)
-    return redirect(url_for('learn_front'))
+    return redirect(url_for("learn_front"))
 
 
 @app.route("/hard_scale", methods=["POST"])
 def hard_scale():
     title = request.form.get('w_title')
     extractor.hard_scale(title)
-    return redirect(url_for('learn_front'))
+    return redirect(url_for("learn_front"))
+
+
+@app.route("/create_card", methods=["POST"])
+def create_card():
+    user_input = request.form.get("inputText")
+    result = extractor.get_definition(user_input)
+    extractor.save_word(result)
+    print(result)
+    if len(result) == 6:
+        message = "added"
+    elif result == "Word already in dictionary":
+        message = "exists"
+    elif result == "Unable to find":
+        message = "wrong word"
+    return render_template("create.html", message=message, word=user_input)
 
 
 if __name__ == "__main__":
