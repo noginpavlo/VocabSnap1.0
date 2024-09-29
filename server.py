468c3c30874a2c720ac24f373e8179e7879b8d31
Nohin Pavlo <nogin.pavlo@lll.kpi.ua>
2024-09-30 00:19:19 +0300

    Home page rebuilt. added new pictures and sections.

--- server.py
+++ server.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 extractor = Extractor()
 
 
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
